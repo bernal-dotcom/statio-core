9ba4b6030a2abde218a74ba4d793dc762c2ef898
bernal-dotcom <cesar.bernal.ti@gmail.com>
2025-06-19 13:14:17 -0600

    Dashboard firt prototype

--- simulation/urls.py
+++ simulation/urls.py
@@ -1,9 +1,12 @@
+from django.urls import path
 from rest_framework.routers import DefaultRouter
-from .views import CrewMemberViewSet, OxygenSystemViewSet, ShipStatusViewSet
+from .views import CrewMemberViewSet, OxygenSystemViewSet, ShipStatusViewSet, sci_fi_dashboard
 
 router = DefaultRouter()
 router.register(r'crew', CrewMemberViewSet)
 router.register(r'oxygen', OxygenSystemViewSet)
 router.register(r'status', ShipStatusViewSet)
 
-urlpatterns = router.urls
+urlpatterns = router.urls + [
+    path('dashboard/', sci_fi_dashboard, name='dashboard'),
+]
--- simulation/views.py
+++ simulation/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import render
 from rest_framework import viewsets
 from .models import CrewMember, OxygenSystem, ShipStatus
 from .serializers import CrewMemberSerializer, OxygenSystemSerializer, ShipStatusSerializer
@@ -15,3 +16,13 @@ class OxygenSystemViewSet(viewsets.ModelViewSet):
 class ShipStatusViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = ShipStatus.objects.all().order_by('-timestamp')[:10]
     serializer_class = ShipStatusSerializer
+
+
+def sci_fi_dashboard(request):
+    context = {
+        "crew_list": CrewMember.objects.all(),
+        "oxygen_list": OxygenSystem.objects.all(),
+        "status_list": ShipStatus.objects.order_by("-timestamp")[:10]
+    }
+
+    return render(request, "dashboard.html", context)
